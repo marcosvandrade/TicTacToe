430a932631a6ffe794bcf351a7e690c80e066a09
marcosvandrade <vascorj2006@gmail.com>
2019-06-01 14:58:40 -0300

    firstCommit

---
+++ App.js
@@ -0,0 +1,94 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Dimensions, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import tictactoe from './src/tictactoe';
+import { typeAlias } from '@babel/types';
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  constructor(props){
+    super(props);
+
+    tictactoe.start();
+
+    this.state = {
+      board: tictactoe.board,
+      gameOver: tictactoe.gameover
+    }
+  }
+
+  makePlay(index){
+
+    tictactoe.make_play(index)
+
+    this.setState({
+      board: tictactoe.board,
+      gameOver: tictactoe.gameover
+    })
+
+  }
+
+  isGameOver{
+    if (this.state.gameOver) {
+      return <text>Game Over</text>  
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        {this.state.board.map((value, index) => (
+          <TouchableOpacity key={index} style={styles.piece}
+            onPress={() => {this.makePlay(index)}}>
+            <text style = {styles.pieceText}>
+              test{value}
+            </text>
+
+          </TouchableOpacity>
+        ))}
+
+        {
+          this.isGameOver()
+        }  
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  piece: {
+    width: Dimensions.get('window').width / 3,
+    height: Dimensions.get('window').width / 3,
+    backgroundColor: "#ddd",
+    alignItems: 'center',
+    alignContent: 'center',
+
+    borderWidth: 0.5,
+    borderColor: "#111" 
+  }
+ 
+  pieceText: {
+    fontSize: 60
+  }
+
+});
